d16d8eabbb42c1be168daccbd8b66c7eb67031a4
borisbotev <boris.botev97@protonmail.com>
2019-01-16 14:58:08 +0200

    Updated program

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -7,11 +7,8 @@ namespace Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-<<<<<<< HEAD
-            Console.WriteLine("C# is cool !!!!");
-=======
             Console.WriteLine("C# is very cool!");
->>>>>>> b162ea810f9f526dc534bd28fd3c24ebd4b269b8
+
         }
     }
 }
